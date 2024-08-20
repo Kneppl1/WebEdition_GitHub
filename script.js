2db730755fa252d8c06133fd82bcd80aeb636f27
Kneppl1 <rm.markert@bbw-fi.de>
2024-08-20 09:48:09 +0200

    training css and js

--- script.js
+++ script.js
@@ -21,3 +21,15 @@ document.getElementById('dark-mode-toggle').addEventListener('click', function()
         link.classList.toggle('dark-mode');
     });
 });
+function toggleDetails(detailsId) {
+    const details = document.getElementById(detailsId);
+    const course = details.parentElement;
+
+    if (details.style.display === "block") {
+        details.style.display = "none";
+        course.style.maxHeight = "250px"; // collapse
+    } else {
+        details.style.display = "block";
+        course.style.maxHeight = "500px"; // expand
+    }
+};
