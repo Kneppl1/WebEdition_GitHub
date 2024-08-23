5f2906ee97eb184ee31007a624ce1ccf88c15d9f
Schubii <144237187+SimonSchubert135@users.noreply.github.com>
2024-08-23 09:17:05 +0200

    Delete news.js

--- news.js
+++
@@ -1,8 +0,0 @@
-// Menü-Toggle-Funktionalität
-const menuToggle = document.getElementById('menu-toggle');
-const sidebar = document.getElementById('sidebar');
-
-menuToggle.addEventListener('click', function() {
-    sidebar.classList.toggle('open');
-    menuToggle.classList.toggle('active');
-});
